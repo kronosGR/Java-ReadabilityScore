9f896462a71f9b1d8189c3c9f8279cc135d8c58b
KronosGR <geo.elgeo@gmail.com>
2022-11-06 12:02:38 +0100

    stage 2/4

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -6,11 +6,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String txt = sc.nextLine();
-        if (txt.length() > 100 ) {
-            System.out.println("HARD");
-        } else if (txt.length() > 0 && txt.length() < 101 ) {
-            System.out.println("EASY");
-        }
+        //String pattern = "^ *\\w+(?: +\\w+){1,10}$";
+        double sentencesL = txt.split("[!.//?]+").length;
+        double wordsL = txt.split(" ").length;
+
+        System.out.println((wordsL / sentencesL <= 10.0 ) ? "EASY" : "HARD");
 
     }
 }
